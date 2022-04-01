a4666cd0b8e78a3602d24b111ba23dd6c0d9bedb
tonikustiana <toni.kustiana@sg-dsa.com>
2022-04-01 20:53:01 +0700

    using secure shared pref

--- checkappversion/src/main/java/id/co/edtslibcheckappversion/di/SharedPreferencesModule.kt
+++ checkappversion/src/main/java/id/co/edtslibcheckappversion/di/SharedPreferencesModule.kt
@@ -1,10 +1,37 @@
 package id.co.edtslibcheckappversion.di
 
-import android.preference.PreferenceManager
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.securepreferences.SecurePreferences
 import org.koin.dsl.module
 
 val sharedPreferencesModule = module {
     single {
-        PreferenceManager.getDefaultSharedPreferences(get())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val spec = KeyGenParameterSpec.Builder(
+                MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+                .build()
+            val masterKey = MasterKey.Builder(get())
+                .setKeyGenParameterSpec(spec)
+                .build()
+
+            EncryptedSharedPreferences.create(
+                get(),
+                "edts_tracker_secret_shared_prefs",
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+        } else {
+            SecurePreferences(get())
+        }
     }
 }
