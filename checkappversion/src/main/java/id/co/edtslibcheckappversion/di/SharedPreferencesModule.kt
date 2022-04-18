1374258c2c7b66233bcd8f6bbb57dc9cde4a1195
Hezbi Islami <muhamad.hezbi@sg-edts.com>
2022-04-18 17:47:41 +0700

    add debuging version shared preferences

--- checkappversion/src/main/java/id/co/edtslibcheckappversion/di/SharedPreferencesModule.kt
+++ checkappversion/src/main/java/id/co/edtslibcheckappversion/di/SharedPreferencesModule.kt
@@ -3,36 +3,45 @@ package id.co.edtslibcheckappversion.di
 import android.os.Build
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import androidx.preference.PreferenceManager
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.securepreferences.SecurePreferences
+import id.co.edtslibcheckappversion.BuildConfig
+import id.co.edtslibcheckappversion.CheckAppVersion
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val sharedPreferencesModule = module {
     single(named("appVersionSharePrefs")) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val spec = KeyGenParameterSpec.Builder(
-                MasterKey.DEFAULT_MASTER_KEY_ALIAS,
-                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-            )
-                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
-                .build()
-            val masterKey = MasterKey.Builder(get())
-                .setKeyGenParameterSpec(spec)
-                .build()
-
-            EncryptedSharedPreferences.create(
-                get(),
-                "edts_checkappversion_secret_shared_prefs",
-                masterKey,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-            )
+        if (CheckAppVersion.debugging) {
+            PreferenceManager.getDefaultSharedPreferences(androidContext())
         } else {
-            SecurePreferences(get())
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val spec = KeyGenParameterSpec.Builder(
+                    MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                )
+                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                    .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+                    .build()
+                val masterKey = MasterKey.Builder(get())
+                    .setKeyGenParameterSpec(spec)
+                    .build()
+
+                EncryptedSharedPreferences.create(
+                    androidContext(),
+                    "edts_checkappversion_secret_shared_prefs",
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                )
+            } else {
+                SecurePreferences(androidContext(), BuildConfig.DB_PASS, "edts_checkappversion_secret_shared_prefs")
+            }
+
         }
     }
 }
